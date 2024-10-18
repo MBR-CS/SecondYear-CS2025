6d5f0c63fec99ce0e26d5a8c653a2540bc35ebd8
Mohamed-Ridha <medbloul2002@gmail.com>
2024-10-18 19:47:29 +0100

    crate the first fucntion in the ex-01 for return the size of linked list linear using cpp

---
+++ Data_Structures_Decode/td-01/ex-01.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std ;  
+  
+
+// create struct node
+struct node;
+using list = node*;
+
+struct node { 
+    int data; 
+    list next; 
+};
+
+
+// create function for take the size and create new linked list 
+list CreateLinkedListLinear(int size) {
+    list head = nullptr;
+    list current = nullptr;
+    
+
+    for (int i = 0; i < size; i++) {
+        list newNode = new node;
+        cout << "Enter the value of node " << i + 1 << ": ";
+        cin >> newNode->data;
+        newNode->next = nullptr;
+        if (head == nullptr)  
+          head = newNode;
+        else  
+          current->next = newNode;
+        current = newNode;
+    }
+    
+    return head;
+}
+
+// create new functino for print the linked list
+void PrintValues(list head){
+  list current = head;
+
+  while (current != nullptr) {
+    cout << current->data <<"  ";
+    current = current->next;
+
+  }
+  cout << endl;
+}
+
+// create function for get size linked list linear
+int SizeLinkedListLinear(list head){
+  int size = 0;
+  list current = head;
+  while (current != nullptr) {
+    size++;
+    current = current->next;
+  }
+  return size;
+}
+
+int  main () {
+  int size;
+  list head;
+  cout << "please enter size of values: ";
+  cin >> size;
+  head = CreateLinkedListLinear(size);
+  PrintValues(head);
+  cout << "the size of linked list is: "<< SizeLinkedListLinear(head)<<" values"<<endl;
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
